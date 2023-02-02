18a1a2562a3018bbb636db57ead4cd40681601e1
DoooggyMarkCHAN <98179643+DoooggyMarkCHAN@users.noreply.github.com>
2023-02-02 22:15:31 +0800

    Add files via upload

---
+++ RemoteSwitch.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;  // 定义Servo对象来控制
+int pos = 0;    // 角度存储变量
+int pos_target = 90;
+int pinNum = D4;
+
+void setup() {
+  myservo.attach(pinNum);  // 控制线连接数字4
+}
+
+void loop() {
+  myservo.write(pos_target); 
+  delay(1000);
+  myservo.write(0); 
+  delay(500);
+  myservo.detach(); 
+  delay(1*60*1000);
+  myservo.attach(pinNum);      
+}
+
+  
